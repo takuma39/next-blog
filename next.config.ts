b467fa0358f26187618a51138cdcb667660b9baa
takuma39 <takuma.sato.job@gmail.com>
2025-03-06 03:43:51 +0900

    feat: Prismaスキーマを作成し、ダミーデータを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'picsum.photos',
+    },
+  ],
+}
 
-export default nextConfig;
+export default nextConfig
