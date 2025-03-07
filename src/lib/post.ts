038d12bf997c8fed069b65c3f30340e2a9ab76ab
takuma39 <takuma.sato.job@gmail.com>
2025-03-08 03:46:01 +0900

    add:記事詳細を追加

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -11,3 +11,14 @@ export async function getPosts() {
     orderBy: { createdAt: 'desc' },
   })
 }
+
+export async function getPost(id: string) {
+  return await prisma.post.findUnique({
+    where: { id },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  })
+}
