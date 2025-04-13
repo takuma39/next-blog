82ee4a04a88cf083727a7d74d8480e0961c7e8ff
takuma39 <takuma.sato.job@gmail.com>
2025-04-13 16:46:23 +0900

    add: 記事投稿ページ作成

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb', // 5MBまでのファイルを受け入れる
+    },
+  },
 }
 
 export default nextConfig
