88d2f637bdfae7132cbe15b38336ceb43da9a674
takuma39 <takuma.sato.job@gmail.com>
2025-04-20 18:23:39 +0900

    add:supabase連携

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,7 +1,18 @@
 import { writeFile } from 'fs/promises'
 import path from 'path'
+import { supabase } from '@/lib/supabase'
 
 export async function saveImage(file: File): Promise<string | null> {
+  const useSupabase = process.env.NEXT_PUBLIC_USE_SUPABASE_STORAGE === 'true'
+
+  if (useSupabase) {
+    return await saveImageToSupabase(file)
+  } else {
+    return await saveImageToLocal(file)
+  }
+}
+
+export async function saveImageToLocal(file: File): Promise<string | null> {
   const buffer = Buffer.from(await file.arrayBuffer())
   const fileName = `${Date.now()}_${file.name}`
   const uploadDir = path.join(process.cwd(), 'public/images')
@@ -11,7 +22,26 @@ export async function saveImage(file: File): Promise<string | null> {
     await writeFile(filePath, buffer)
     return `/images/${fileName}`
   } catch (error) {
-    console.error('Error saving image:', error)
+    console.error('画像保存エラー', error)
     return null
   }
+}
+
+async function saveImageToSupabase(file: File): Promise<string | null> {
+  const fileName = `${Date.now()}_${file.name}`
+  const { error } = await supabase.storage.from('udemy-next-blog-bucket').upload(fileName, file, {
+    cacheControl: '3600',
+    upsert: false,
+  })
+
+  if (error) {
+    console.error('Upload error:', error.message)
+    return null
+  }
+
+  const { data: publicUrlData } = supabase.storage
+    .from('udemy-next-blog-bucket')
+    .getPublicUrl(fileName)
+
+  return publicUrlData.publicUrl
 }
